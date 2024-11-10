77a2d7f72da60d573062705f9c402aface5b8653
GChukwudi <g.chukwudi@alustudent.com>
2024-11-10 18:04:51 +0200

    dev underway

---
+++ lib/utils/icons.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+
+class Icons {
+  Icons._();
+
+  static const _kFontFam = 'Icons';
+  static const String? _kFontPkg = null;
+
+  static const IconData lightbulb =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_up =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData heart =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData link =
+      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData notification_bing =
+      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData play =
+      IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search_normal =
+      IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData star =
+      IconData(0xe80c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user =
+      IconData(0xe80d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user_add =
+      IconData(0xe80e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_down =
+      IconData(0xe810, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_left =
+      IconData(0xe811, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_right =
+      IconData(0xe812, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData download =
+      IconData(0xe865, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData facebook_1 =
+      IconData(0xf09a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData apple =
+      IconData(0xf179, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData google =
+      IconData(0xf1a0, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData shopping_bag =
+      IconData(0xf290, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pen =
+      IconData(0xf304, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
